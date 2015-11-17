37dd7b9620163bf3f7f571ec6d92e56f8458dfc7
guillaume Nostrenoff <g.nostrenoff@gmail.com>
2015-11-17 17:30:25 +0100

    Added new module, controller and service files (tp step 3) + a new index html.

---
+++ login/js/controllers/EventController-s3.1.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('adminApp').controller('eventCtrl',loginCrtFnt);
+
+loginCrtFnt.$inject=['$scope','$log', 'auth', '$window'];
+
+function loginCrtFnt($scope, $log, auth, $window){ 
+	$scope.logAuth = function() {
+
+		if($scope.user == undefined)
+			$log.info('user info needed!');
+		else{
+			$log.info('user login', $scope.user.login); 
+			$log.info('user pwd', $scope.user.pwd);
+		}
+
+		//authentication process
+		auth.authAsk($scope.user.login, $scope.user.pwd).then(
+			function(data){
+				if(data.role == 'admin')
+					$window.location.href = 'http://www.google.com';
+				else if(data.role == 'watcher')
+					$window.location.href = 'https://www.facebook.com/';
+				//should not happen 
+				else
+					$window.alert('you are registered but we dont know who you are');
+			},
+			function(data){
+				$window.alert(data);
+			}
+		);
+	
+	}; 
+
+}
