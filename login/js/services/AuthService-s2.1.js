745c12f1cc44531ad74f388933bc044e4d0126ad
louis <amoros.louis@gmail.com>
2015-12-08 18:18:18 +0100

    Auto indent code.

--- login/js/services/AuthService-s2.1.js
+++ login/js/services/AuthService-s2.1.js
@@ -4,25 +4,25 @@ angular.module('authService', []).service('auth',authFnc);
 
 authFnc.$inject=['$http', '$q'];
 
-function authFnc($http, $q) { 
+function authFnc($http, $q) {
 
 	//key is the login, value is a table with respectivelly pwd and role
 	var userMap={};
-	userMap['jdoe']=['jdoepwd', 'admin']; 
-	userMap['psmith']=['psmithpwd', 'watcher']; 
+	userMap['jdoe']=['jdoepwd', 'admin'];
+	userMap['psmith']=['psmithpwd', 'watcher'];
 	userMap['tp']=['tp', 'watcher'];
 
-	var fncContainer={ 
-		authAsk:authAsk 
+	var fncContainer={
+		authAsk:authAsk
 	};
 
-	
-	function authAsk(login,pwd){ 
 
-		var deferred = $q.defer(); 
+	function authAsk(login,pwd){
 
-		$http.post('http://localhost:5000/',{'login':login,'pwd':pwd}).
-			success(function(data, status, headers, config) { 
+		var deferred = $q.defer();
+
+		$http.post('//localhost:5000/connect',{'login':login,'pwd':pwd}).
+			success(function(data, status, headers, config) {
 				//user successfully logged
 				console.log('good jooooooooooooooooob');
 				console.log(data);
@@ -41,5 +41,5 @@ function authFnc($http, $q) {
         return deferred.promise;
     }
 
-	return fncContainer; 
+	return fncContainer;
 }
