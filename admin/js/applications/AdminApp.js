f16c3f23996ee7c5c55cbc4eef69c72dae7bdc47
louis <amoros.louis@gmail.com>
2015-12-13 19:24:26 +0100

    Redo watcher design and clean code.

--- admin/js/applications/AdminApp.js
+++ admin/js/applications/AdminApp.js
@@ -1,3 +1,19 @@
 'use strict';
 
-angular.module('adminApp', ['ngDraggable','factoryServices','commServices']);
+angular
+  .module('adminApp', ['ngDraggable','factoryServices','commServices','ngRoute'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/dashboard', {
+        templateUrl: 'index.html',
+        controller: 'AdminCtrl'
+      })
+      .when('/presentation', {
+        templateUrl: 'views/presentation.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/dashboard'
+      });
+  });
---
+++ watcher/js/applications/WatcherApp.js
@@ -0,0 +1,3 @@
+'use strict';
+
+angular.module('watcherApp', ['commServices','factoryServices']);
